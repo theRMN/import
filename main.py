707f9f66bb6432532e35f80f5de4c9b24ccd2bd3
theRMN <roman.vassev@gmail.com>
2021-04-29 12:44:04 +0600

    import datetime

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from application.salary import rr
 from application.db import people
+from datetime import datetime
 
 if __name__ == '__main__':
     rr()
     people.pp()
+    print(datetime.now(tz=None))
